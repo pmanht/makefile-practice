59cd54614d6d5917e4cdf7b75c0e16e6b6a2eca0
Tuan <pmanht@gmail.com>
2025-05-09 09:22:01 +0000

    handle make install for app that use dynamic library

---
+++ techniques/stage08/src/example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "example.h"
+#include "file01.h"
+#include "file02.h"
+#include "file03.h"
+#include "file04.h"
+#include "file05.h"
+#include "file06.h"
+
+int main(void) {
+	func01();
+	func02();
+	func03();
+	
+	func04();
+	func05();
+	func06();
+}
